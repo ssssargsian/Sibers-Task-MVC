d668ddce097d4e92aef48cbe5a4605d636c9f325
ssssargsian <ssssargsian@gmail.com>
2021-07-23 00:40:22 +0700

    Добавьте файлы проекта.

---
+++ Sibers-Task-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sibers_Task_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
