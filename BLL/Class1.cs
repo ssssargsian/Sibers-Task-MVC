8e25bc304ccd74354394212dfedce20fb8d6dd1e
ssssargsian <ssssargsian@gmail.com>
2021-07-23 21:17:16 +0700

    1

--- BLL/Class1.cs
+++ BLL/Class1.cs
@@ -5,4 +5,4 @@ namespace BLL
     public class Class1
     {
     }
-}
+} 
---
+++ Sibers-Task-MVC/Models/Projects.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sibers_Task_MVC.Models
+{
+    public class Projects
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string  NameCompanyClient { get; set; }
+        public string  MyProperty { get; set; }
+    }
+}
